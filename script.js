37c22c6134069f3847985dd4cc73dd9cdf3504eb
kwagm <kwagm@amazon.com>
2021-11-01 20:41:01 -0400

    ...

---
+++ script.js
@@ -0,0 +1,29 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+
+    if (operator === "+") {
+        return a + b;
+    } else if (operator === "-") {
+        return a - b;
+    } else if (operator === "*") {
+        return a * b;
+    } else if (operator === "/") {
+        return a / b;
+    }
+}
